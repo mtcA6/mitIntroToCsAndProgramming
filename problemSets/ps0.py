9fc8e57f91804c7fbff9a8dd9e374e9bef49a45c
mtca6 <129431463+mtcA6@users.noreply.github.com>
2024-10-24 07:09:42 -0400

    complete ps0

--- problemSets/ps0.py
+++ problemSets/ps0.py
@@ -1,3 +1,20 @@
-## ps0
+# Problem Set 0
+# Name: Travis
+# Collaborators: n/a
+# Time Spent: 2hrs - couldn't get PyCharm to connect to GitHub w my token.
 
-## test test
+## 4.3.1) Assignment
+## Write a program that does the following in order:
+## 1. At the top of your file and type: import numpy
+# 2. Now write a line that sets a variable named x to 5.
+# 3. Now write a line that sets a variable named y to 8.
+# 4. Add variables xand y, and save the result to a variable named z.
+# 5. Now save the result of this command: numpy.log2(z) to a variable named a.
+
+import numpy
+
+x = 5
+y = 8
+z = x + y
+a = numpy.log2(z)
+print(a)
